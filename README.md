cd3d25a49f10c68ca7e0f254e10191575d1de547
Nika Serif <nika@nika.sh>
2024-06-09 07:01:24 +0300

    0.2.0: add find in files, update desc

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
-# QuickOpen Workspace
+# Monorepo Navigator
 
-This is a simple extension that opens `Quick access` menu with predefined workspace path.
+Adds commands to open files or seach inside them in relative workspace in monorepo.
+
+Let's say your monorepo has this structure:
+
+```
+pkgs
+	design-system
+		button
+		grid
+		modal
+```
+
+When you open `Button.tsx` file, you can use commands from this extension to narrow down Quick Open (cmd + p), or Find in files (cmd + shift + f) to only seach within `button` monorepo package.
+
+## How to use commands:
 
 - Open the command palette
-- Run "Quick Open Presets"
-- Pick one of the presets and see it run
+- Run `Monorepo Naviagator: Quick Open` or `Monorepo Navigator: Find in files`.
 
 ## Demo
 
@@ -12,17 +25,22 @@ This is a simple extension that opens `Quick access` menu with predefined worksp
 
 ## How it works
 
-The extension uses the [`workbench.action.quickOpen`](https://code.visualstudio.com/api/references/commands) command to suggest files.
+The extension uses the [`workbench.action.quickOpen` and `workbench.action.findInFiles`](https://code.visualstudio.com/api/references/commands) commands to insert the path to the closest package relative to the currently open file.
 
 ## VS Code API
 
 ### `vscode` module
 
 - [`workbench.action.quickOpen`](https://code.visualstudio.com/api/references/commands)
+- [`workbench.action.findInFiles`](https://code.visualstudio.com/api/references/commands)
 
-# How to run locally
+## How to run locally
 
 - Run `npm install` in terminal to install dependencies
 - Run the `Run Extension` target in the Debug View. This will:
-	- Start a task `npm: watch` to compile the code
-	- Run the extension in a new VS Code window
+  - Start a task `npm: watch` to compile the code
+  - Run the extension in a new VS Code window
+
+## Credits
+
+Icon by [Iconic Panda](https://www.flaticon.com/authors/iconic-panda) on FlatIcon.
--- src/extension.ts
+++ src/extension.ts
@@ -2,34 +2,69 @@ import * as vscode from "vscode";
 
 import { findRootDir } from "./utils";
 
+function findSearchPath(
+  editor: vscode.TextEditor,
+  action: "quickOpen" | "findInFiles"
+) {
+  const activeFilePath = vscode.window.activeTextEditor?.document.uri.fsPath;
+  const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+
+  const fallbackAction =
+    action === "quickOpen"
+      ? "workbench.action.quickOpen"
+      : "workbench.action.findInFiles";
+
+  if (!workspaceFolder || !activeFilePath) {
+    vscode.commands.executeCommand(fallbackAction);
+
+    return;
+  }
+
+  const rootPath = findRootDir(activeFilePath);
+
+  if (!rootPath) {
+    vscode.commands.executeCommand(fallbackAction);
+
+    return;
+  }
+
+  const searchPath = `${rootPath}/`.replace(
+    `${workspaceFolder.uri.fsPath}/`,
+    ""
+  );
+
+  return searchPath;
+}
+
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand("quickOpen.workspace", async () => {
-      const activeFilePath =
-        vscode.window.activeTextEditor?.document.uri.fsPath;
-
-      const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
-
-      if (!workspaceFolder || !activeFilePath) {
-        vscode.commands.executeCommand("workbench.action.quickOpen");
-
-        return;
-      }
-
-      const rootPath = findRootDir(activeFilePath);
-
-      if (!rootPath) {
-        vscode.commands.executeCommand("workbench.action.quickOpen");
-
-        return;
-      }
-
-      const searchPath = `${rootPath}/`.replace(
-        `${workspaceFolder.uri.fsPath}/`,
-        "",
-      );
-
-      vscode.commands.executeCommand("workbench.action.quickOpen", searchPath);
-    }),
+    vscode.commands.registerCommand(
+      "monorepoNavigator.quickOpen",
+      cmdOpenFileInRelativeWorkspace
+    ),
+    vscode.commands.registerTextEditorCommand(
+      "monorepoNavigator.findInFiles",
+      cmdFindInFilesInRelativeWorkspace
+    )
   );
+}
+
+function cmdOpenFileInRelativeWorkspace(
+  editor: vscode.TextEditor,
+  _edit: vscode.TextEditorEdit
+) {
+  const path = findSearchPath(editor, "quickOpen");
+  return vscode.commands.executeCommand("workbench.action.quickOpen", path);
+}
+
+function cmdFindInFilesInRelativeWorkspace(
+  editor: vscode.TextEditor,
+  _edit: vscode.TextEditorEdit
+) {
+  const path = findSearchPath(editor, "findInFiles");
+
+  return vscode.commands.executeCommand("workbench.action.findInFiles", {
+    query: "",
+    filesToInclude: path,
+  });
 }
